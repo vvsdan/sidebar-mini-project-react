80ffb32eed7f4ea1057599b3362c1d1a69651e57
Daniel Jones <vvsdan999@gmail.com>
2023-10-30 11:43:06 -0400

    add completed project from local

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <>
+      <Modal />
+      <Home />
+      <Sidebar />
+    </>
+  );
+};
+export default App;
